978fc6e976600f3bfb7d879d9ff478e801c6748f
wangyongtao <wangyt4dev@gmail.com>
2018-10-30 11:44:43 +0800

    加入主机名和IP.

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,20 @@
 package hello;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController
 public class HelloController {
-    
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
-    
+
+  @RequestMapping("/")
+  public String index() throws UnknownHostException {
+    InetAddress ia = InetAddress.getLocalHost();
+    String host = ia.getHostName();
+    String IP = ia.getHostAddress();
+    System.out.println("Host," + host + ";IP," + IP);
+    return "Greetings from Spring Boot!";
+  }
 }
