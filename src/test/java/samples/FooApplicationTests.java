2233e3198952de84b6b624cd826e9bb47fba2adb
wangyongtao <wangyt4dev@gmail.com>
2019-07-05 15:36:45 +0800

    增加Tests

---
+++ src/test/java/samples/FooApplicationTests.java
@@ -0,0 +1,16 @@
+package samples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FooApplicationTests {
+
+  @Test
+  public void contextLoads() {
+    //
+  }
+}
