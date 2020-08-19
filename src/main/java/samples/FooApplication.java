3638ad931dd0980dfe2046819863570f5a68d184
wangyongtao <wangyt4dev@gmail.com>
2020-08-19 20:23:58 +0800

    引入YCRedis-SDK

--- src/main/java/samples/FooApplication.java
+++ src/main/java/samples/FooApplication.java
@@ -1,17 +1,17 @@
 package samples;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import samples.mapper.EmpMapper;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.Arrays;
 
 @Slf4j
 @SpringBootApplication
+@ComponentScan(basePackages = {"samples", "com.yiche.redis.api"})
 public class FooApplication implements CommandLineRunner {
 
   public static void main(String[] args) {
