923ac0edfa76fcf313d372d8055219939f934e29
wangyongtao <wangyt4dev@gmail.com>
2020-09-18 10:10:32 +0800

    opt.

--- src/main/java/samples/FooApplication.java
+++ src/main/java/samples/FooApplication.java
@@ -4,12 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 
 import java.util.Arrays;
 
 @Slf4j
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FooApplication implements CommandLineRunner {
 
   public static void main(String[] args) {
